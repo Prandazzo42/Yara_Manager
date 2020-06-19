a9bc45d33f22e88671ddfc4ae87fae6160badbbb
eupho <eupho.42@gmail.com>
2020-06-20 01:52:52 +0200

    app creation

--- Yara_manager/settings.py
+++ Yara_manager/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Yara_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "ruleManager/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
